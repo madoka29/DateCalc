bbe0985cd7cd33569463dbe35335b2819e9af20c
madoka29 <tomatoice29@gmail.com>
2019-06-11 21:50:26 +0900

    First Commit

---
+++ src/main/java/com/example/demo/DateCalcApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateCalcApplication.class, args);
+	}
+
+}
