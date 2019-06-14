f44b9333e878161d9aab9c87c24c47b6cfa06ce4
madoka29 <tomatoice29@gmail.com>
2019-06-14 22:59:52 +0900

    .gitignoreファイル修正、CRUD処理を作成

---
+++ src/main/java/com/example/demo/domain/DateCalcMapper.java
@@ -0,0 +1,35 @@
+package com.example.demo.domain;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+// オブジェクトとデータベースを紐づける役割を持つMapper
+
+@Mapper
+public interface DateCalcMapper {
+
+    // 日付計算用データを検索して1件取得する
+    @Select("SELECT * FROM date_calc WHERE dateId = #{dateId}")
+    public DateCalcData dateCalcSelectOne(int dateId);
+
+    // 日付計算用データをすべて表示する
+    @Select("SELECT * FROM date_calc")
+    public List<DateCalcData> dateCalcSelectAll();
+
+    // 日付計算用データを登録する
+    @Insert("INSERT INTO date_calc(dateId, dateName, plusLessYear, plusLessMonth, plusLessDay) VALUES (null, #{dateName}, #{plusLessYear}, #{plusLessMonth}, #{plusLessDay});")
+    public boolean dateCalcInsert(DateCalcData dcd);
+
+    // 日付計算用データを更新する
+    @Update("UPDATE date_calc SET dateName = #{dateName}, plusLessYear = #{plusLessYear}, plusLessMonth = #{plusLessMonth}, plusLessDay = #{plusLessDay} WHERE dateId = #{dateId}")
+    public boolean dateCalcUpdate(DateCalcData dcd);
+
+    // 日付計算用データを削除する
+    @Delete("DELETE FROM date_calc WHERE dateId = #{dateId}")
+    public boolean dateCalcDelete(int dateId);
+}
